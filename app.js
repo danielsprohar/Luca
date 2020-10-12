0ca04bc195a33e339c17c59546171699de40899b
Daniel Sprohar <daniel.sprohar@outlook.com>
2020-10-11 23:14:39 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+
+var app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
