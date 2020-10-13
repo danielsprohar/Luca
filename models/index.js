2a868e1ced2ad4557ab0d460bcabcafa1328c57b
Daniel Sprohar <daniel.sprohar@outlook.com>
2020-10-13 15:44:07 -0500

    Initial relationships

--- models/index.js
+++ models/index.js
@@ -1,14 +1,47 @@
-const models = {
-  CustomerVehicle: require('./customer-vehicle'),
-  Customer: require('./customer'),
-  Invoice: require('./invoice'),
-  ParkingSpace: require('./parking-space'),
-  Payment: require('./payment'),
-  RentalAgreement: require('./rental-agreement'),
-  Role: require('./role'),
-  User: require('./user').User
-}
+const CustomerVehicle = require('./customer-vehicle')
+const Customer = require('./customer')
+const Invoice = require('./invoice')
+const ParkingSpace = require('./parking-space')
+const Payment = require('./payment')
+const RentalAgreement = require('./rental-agreement')
+const Role = require('./role')
+const User = require('./user').User
 
-// TODO: Create relationships
+// ===========================================================================
+// Many-to-Many
+// ===========================================================================
+
+User.belongsToMany(Role, { through: 'user_roles' })
+User.belongsToMany(Role, { through: 'user_roles' })
+
+Customer.belongsToMany(ParkingSpace, { through: 'parking_space_occupants' })
+ParkingSpace.belongsToMany(Customer, { through: 'parking_space_occupants' })
+
+Invoice.belongsToMany(Payment, { through: 'invoice_payments' })
+Payment.belongsToMany(Invoice, { through: 'invoice_payments' })
+
+// ===========================================================================
+// One-to-Many
+// ===========================================================================
+RentalAgreement.belongsTo(ParkingSpace)
+RentalAgreement.belongsTo(Customer)
+
+Invoice.belongsTo(RentalAgreement)
+
+CustomerVehicle.belongsTo(Customer)
+
+
+// ===========================================================================
+
+const models = {
+  CustomerVehicle,
+  Customer,
+  Invoice,
+  ParkingSpace,
+  Payment,
+  RentalAgreement,
+  Role,
+  User
+}
 
 module.exports = models
